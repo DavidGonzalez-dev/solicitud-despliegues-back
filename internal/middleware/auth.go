18de109e6a7a1e0edfb09317d28e1a17f4104c7d
David Felipe Gonzalez Cuevas <davidfgonzalezc1@gmail.com>
2025-09-01 15:24:31 -0500

    feat: middleware for validating entra ID generated access tokens

---
+++ internal/middleware/auth.go
@@ -0,0 +1,79 @@
+package customMiddleware
+
+import (
+	"go-solicitud-despliegues-back/internal/config"
+	customContext "go-solicitud-despliegues-back/pkg/context"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+
+	"github.com/labstack/echo/v4"
+)
+
+// Middleware returns the echo.MiddlewareFunc that validates the token and puts the user in the context.
+// Validations performed:
+// - Authorization header present and with Bearer
+// - valid signature against JWKS
+// - issuer == expected
+// - audience contains ApiAudience
+// - exp (expiration) not expired
+// - exposes useful claims in ContextUser
+func JWTValidatorMiddleware(authenticator *config.Authenticator) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+
+			// Get token from header
+			authHeader := c.Request().Header.Get("Authorization")
+			if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+				println("Missing or invalid Authorization header")
+				return c.JSON(http.StatusUnauthorized, echo.Map{"error": "Unauthorized"})
+			}
+			tokenString := authHeader[len("Bearer "):]
+
+			// Parse and validate
+			token, err := jwt.Parse(tokenString, authenticator.JWKS.Keyfunc)
+			if err != nil {
+				println("Error parsing token:", err.Error())
+				return c.JSON(http.StatusUnauthorized, echo.Map{"error": "Unauthorized"})
+			}
+
+			// Read Claims
+			claims, ok := token.Claims.(jwt.MapClaims)
+			if !ok {
+				println("Invalid token claims")
+				return c.JSON(http.StatusUnauthorized, echo.Map{"error": "Unauthorized"})
+			}
+
+			if exp, ok := claims["exp"].(float64); ok {
+				if time.Unix(int64(exp), 0).Before(time.Now()) {
+					println("Token has expired")
+					return c.JSON(http.StatusUnauthorized, echo.Map{"error": "Unauthorized"})
+				}
+			}
+
+			// Validate claims
+			if claims["aud"] != authenticator.Audience {
+				println("Invalid audience")
+				return c.JSON(http.StatusUnauthorized, echo.Map{"error": "Unauthorized"})
+			}
+			if claims["iss"] != authenticator.Issuer {
+				println("Invalid issuer")
+				return c.JSON(http.StatusUnauthorized, echo.Map{"error": "Unauthorized"})
+			}
+
+			// Store Map claims as contextUser
+			contextUser := customContext.ContextUser{
+				AccessToken: tokenString,
+				Subject:     claims["sub"].(string),
+				OID:         claims["oid"].(string),
+				Roles:       claims["roles"].([]any),
+				Raw:         claims,
+			}
+			c.Set(customContext.UserCtxKey, contextUser)
+			return next(c)
+
+		}
+	}
+}
